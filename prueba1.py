4cf16ffa2b3fad73aa111f30360d4ece8bf15a82
Teret90 <157057098+Teret90@users.noreply.github.com>
2024-03-06 13:35:38 +0100

    troncho2

--- prueba1.py
+++ prueba1.py
@@ -2,3 +2,26 @@ lista = [10,15,20]
 
 for i in lista:
     print(i)
+
+
+print("Hola mundo")
+if print is True:
+    lista = [10,15,20]
+
+for i in lista:
+    print(i)
+lista = [10,20,5,28,30]
+print("10")
+print("14342425")
+for i in lista:
+    print(i/2) 
+    print("hohahalahahah")
+
+lista = [1,54,22,30]
+for i in lista:
+    print(i*2)
+    print("hola") 
+
+lista = [16,20,30]
+for i in lista:
+    print(i)
